d9451f4fe8867450c2146a4c40694e7c2af4b804
singhharjot011 <singhharjot011@gmail.com>
2024-05-04 13:53:58 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector(".toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+mobileMenu.classList.add("hidden");
+toggleButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
